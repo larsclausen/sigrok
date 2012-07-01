f324a19319fced07b512818e74d264b8f5139f91
Lars-Peter Clausen <lars@metafoo.de>
2012-07-01 21:04:36 +0200

    sr: session: Close a device when it is removed from a session
    A device is opened when it is added to a session, in the same fashion it should
    be closed again when it is removed from a session.
    
    Also remove all still attached devices from a session when the session is
    destroyed.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- libsigrok/session.c
+++ libsigrok/session.c
@@ -82,8 +82,7 @@ SR_API int sr_session_destroy(void)
 		return SR_ERR_BUG;
 	}
 
-	g_slist_free(session->devs);
-	session->devs = NULL;
+	sr_session_dev_remove_all();
 
 	/* TODO: Error checks needed? */
 
@@ -93,6 +92,12 @@ SR_API int sr_session_destroy(void)
 	session = NULL;
 
 	return SR_OK;
+}
+
+static void sr_dev_close(struct sr_dev *dev)
+{
+	if (dev->driver->dev_close)
+		dev->driver->dev_close(dev->driver_index);
 }
 
 /**
@@ -110,7 +115,7 @@ SR_API int sr_session_dev_remove_all(void)
 		return SR_ERR_BUG;
 	}
 
-	g_slist_free(session->devs);
+	g_slist_free_full(session->devs, (GDestroyNotify)sr_dev_close);
 	session->devs = NULL;
 
 	return SR_OK;
